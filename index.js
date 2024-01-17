fec5ce9bbd2c042c96742c1f936d396e470426e8
Kruti Newalkar <82303924+itskrutinewalkar@users.noreply.github.com>
2024-01-17 17:51:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const apiKey = "openuv-kscrll9cx8z8-io";
+const config = {
+    headers: {
+        'x-access-token' : 'openuv-kscrll9cx8z8-io',
+    }
+};
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.post("/", async (req, res) => {
+    const latitude = req.body.latitude;
+    const longitude = req.body.longitude;
+
+    try {
+     const response = await axios.get( `https://api.openuv.io/api/v1/uv?lat=${latitude}&lng=${longitude}`, {
+        headers: {
+            'x-access-token' : apiKey,
+        }
+     });
+    
+      const uv_index = response.data.result.uv;
+      res.render("result.ejs", {content: uv_index});
+    }
+
+    catch(error) {
+        res.send(error.response.data);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
